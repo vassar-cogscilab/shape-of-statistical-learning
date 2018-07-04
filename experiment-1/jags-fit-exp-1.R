0f8807ee66580bdc1825d29b455210741db825f3
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-07-04 13:42:29 -0400

    latent learner model

--- experiment-1/jags-fit-exp-1.R
+++ experiment-1/jags-fit-exp-1.R
@@ -21,10 +21,10 @@ data.for.jags <- list(
   n.pairs = (data %>% group_by(subject_id) %>% summarise(n.pairs = length(unique(pair))) %>% select(n.pairs) %>% as.matrix)[,1]
 )
 
-params.to.monitor <- c('sigma', 'alpha', 'gamma', 'beta', 'beta.learn', 'gamma.learn', 'delta')
+params.to.monitor <- c('sigma', 'alpha', 'gamma', 'beta', 'beta.learn', 'gamma.learn', 'delta', 'is.learner')
 
-jags.result <- run.jags('JAGSmodels/jags-model-expexp.txt', monitor=params.to.monitor, data=data.for.jags, n.chains=2,
-                        burnin=1000, sample=5000, adapt=2000)
+jags.result <- run.jags('JAGSmodels/jags-model-expexp-latent-learner.txt', monitor=params.to.monitor, data=data.for.jags, n.chains=2,
+                        burnin=1000, sample=5000, adapt=2000, thin=2, method="para")
 
 result <- as.matrix(as.mcmc(jags.result))
 save(jags.result, file = 'experiment-1/data/generated/jags/example_model_output.Rdata')
