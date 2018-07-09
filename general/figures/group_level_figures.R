7ef8ebe4327c866eb0f2e4b15afad0ce7fd73a75
FibonacciGirl <isdestefano@vassar.edu>
2018-07-09 11:16:57 -0400

    Group level

---
+++ general/figures/group_level_figures.R
@@ -0,0 +1,102 @@
+library(tidyr)
+library(dplyr)
+
+
+#experiment 1
+data.all <- read.csv('experiment-1/data/raw/all-data.csv')
+
+test_data<- data.all %>% filter(practice == 0) %>%
+  mutate(t = rep(as.vector(sapply(0:71, function(x){return(rep(x,12))})), length(unique(subject))))%>%
+  filter(correct==1 & triple_type == 'critical' & triple_position%in%c(1,3))%>% 
+  mutate(subject_id = as.numeric(factor(subject)),
+         subject_condition = factor(as.character(cond), levels=c('known','unknown','one')),
+         is_predictable = as.numeric(factor(triple_position))-1)%>%
+  group_by(is_predictable)%>%
+  mutate(global_index = ((as.numeric(block)+1)*t)/2)
+
+
+plot_data <- test_data %>% mutate(predictable = if_else(is_predictable == 0, 'unpredictable', 'predictable'))
+
+
+library(ggplot2)
+ggplot(plot_data)+
+  geom_smooth(aes(x=global_index, y= rt, col= predictable))+
+  facet_grid(cond~.)
+
+#experiment 2
+library(readr)
+data.all <- read_csv2('experiment-2/data/raw/sl_size_data.csv')
+
+
+set.size.condition <- data.all %>% filter(sequence_type == 'predictable') %>% 
+  group_by(subject_id, grid) %>% 
+  summarize() %>% ungroup() %>%
+  mutate(grid.fct = as.numeric(factor(grid)),
+         set_size = grid.fct * 2 + 2) %>% 
+  select(subject_id, set_size)
+
+test.data <- data.all %>% inner_join(set.size.condition)%>%
+  filter(sequence_type == 'unpredictable' | sequence_type == 'predictable') %>% 
+  select(subject_id, set_size, block, rt, correct, target, sequence_type)
+
+## add column for which pair, which target
+
+subject.pairs <- test.data %>% group_by(subject_id) %>% 
+  filter(row_number() <= 16) %>% 
+  select(subject_id, target) %>%
+  mutate(pair = rep(c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8), length(unique(subject_id))))%>% 
+  group_by(subject_id, target) %>% 
+  filter(row_number() <= 1) %>% 
+  ungroup() %>% group_by(subject_id) %>% 
+  mutate(target_index = 1:n()) %>% 
+  ungroup()
+
+test.data <- test.data %>% left_join(subject.pairs, by=c('subject_id', 'target'))%>% 
+  group_by(subject_id, target) %>% mutate(t = 1:n()) %>% ungroup() %>% select(-target) %>%
+  mutate(subject_id = as.numeric(factor(test.data$subject_id)),
+  is_predictable = as.numeric(factor(test.data$sequence_type, levels=c('unpredictable', 'predictable'))) - 1) %>%
+  filter(correct == 1) %>% mutate(subject_condition = as.numeric(factor(set_size)))%>%
+  group_by(is_predictable)%>%
+  mutate(global_index = ((as.numeric(block)+1)*t))
+
+
+plot_data <- test.data %>%   mutate(predictable = if_else(is_predictable == 0, 'unpredictable', 'predictable'))%>%
+  mutate(set_size = paste0(set_size,' pairs'))
+ggplot(plot_data)+
+  geom_smooth(aes(x=global_index, y= rt, col= predictable))+
+  facet_grid(set_size~.)
+
+
+#experiment 3
+
+data.all <- read_csv('experiment-3/data/raw/sl_mouse_click_data.csv')
+
+
+test.data <- data.all %>% filter(phase=='rt-test') %>% 
+  group_by(prolific_pid, pair, target_type) %>% mutate(t = 1:n()) %>% ungroup() 
+
+target.id <- test.data %>% filter(t==1) %>% group_by(prolific_pid) %>% mutate(target_id = 1:n()) %>% ungroup() %>% select(prolific_pid, pair, target_type, target_id)
+subject.condition <- target.id %>% group_by(prolific_pid) %>% summarize(max = n()) %>% mutate(subject_condition = as.numeric(factor(max))) %>% select(prolific_pid, subject_condition)
+
+test.data = test.data %>% 
+  inner_join(target.id)%>% 
+  inner_join(subject.condition)%>%
+  mutate(subject_id = as.numeric(factor(prolific_pid))) %>%
+  mutate(pair_id = pair) %>%
+  mutate(rt=as.numeric(rt)) %>%
+  mutate(is_predictable = as.numeric(factor(target_type, levels=c('unpredictable', 'predictable'))) - 1)%>%
+  mutate(global_index = ((as.numeric(block)+1)*t))
+
+plot_data <- test.data %>% mutate(predictable = if_else(is_predictable == 0, 'unpredictable', 'predictable'))%>%
+  mutate(set_size = paste0(subject_condition,' pairs'))
+
+ggplot(plot_data)+
+  geom_smooth(aes(x=global_index, y= rt, col= predictable))+
+  facet_grid(set_size~.)
+
+block_data<- plot_data %>% group_by(set_size,predictable,block, subject_id) %>% summarise(mean_rt = mean(rt)) %>% ungroup()
+ ggplot(block_data)+
+   geom_boxplot(aes(x=block, y = mean_rt, col= predictable))+
+   facet_grid(set_size~.)+
+   ylim(0,2000)
+
