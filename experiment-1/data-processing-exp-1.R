7e32207f529f7a795760e30c5642b419ca355118
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-07-10 10:15:57 -0400

    getting set for junior

--- experiment-1/data-processing-exp-1.R
+++ experiment-1/data-processing-exp-1.R
@@ -12,7 +12,7 @@ data.test <- data.all %>% filter(practice==0)
 
 data.test$t <- rep(as.vector(sapply(0:71, function(x){return(rep(x,12))})), n_subjects)
 
-# JAGS data only ####
+# JAGS-relevant data only ####
 
 data.for.model <- subset(data.test, correct==1 & triple_type == 'critical' & triple_position%in%c(1,3), c('subject','cond','t','triple_position','rt'))
 
@@ -26,7 +26,24 @@ data.for.model$subject_condition <- as.numeric(data.for.model$cond)
 data.for.model <- data.for.model %>% mutate(is_predictable = as.numeric(factor(triple_position))-1, target_index = is_predictable+1, pair=1) %>%
   select(subject_id, rt, pair, target_index, t, is_predictable, subject_condition)
 
-# output csv for JAGS
+# output csv for plotting
 
-write_csv(data.for.model, path = "experiment-1/data/generated/for-jags-exp-1.csv")
+write_csv(data.for.model, path = "experiment-1/data/generated/simplified-exp-1.csv")
+
+# generate list for jags
+
+data.for.jags <- list(
+  rt = data.for.model$rt,
+  subject_id = data.for.model$subject_id,
+  is.predictable = data.for.model$is_predictable,
+  t = data.for.model$t,
+  N = length(data.for.model$rt),
+  S = length(unique(data.for.model$subject_id)),
+  C = length(unique(data.for.model$subject_condition)),
+  condition = data.for.model$subject_condition,
+  max_t = (data.for.model %>% group_by(subject_id) %>% summarise(max_t = max(t)) %>% select(max_t) %>% as.matrix)[,1],
+  n.pairs = (data.for.model %>% group_by(subject_id) %>% summarise(n.pairs = length(unique(pair))) %>% select(n.pairs) %>% as.matrix)[,1]
+)
+
+save(data.for.jags, file="experiment-1/data/generated/jags-data-exp-1.Rdata")
 
